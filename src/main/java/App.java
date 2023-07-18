e14dd3137aa968be0cb1f865b530da38e402bc58
Alexey Kabakov <kabakov_nl@mail.ru>
2023-07-18 19:20:48 +0300

    Выполнено!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = applicationContext.getBean("catBean",Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean",Cat.class);
+        System.out.println("Сравнение Объектов HelloWorld: "+ (bean1==bean2)+"\n"+bean1+"\n" +bean2);
+        System.out.println("Сравнение Объектов Cat: "+ (cat1==cat2)+"\n"+cat1+"\n" +cat2);
+
+
+
     }
 }
